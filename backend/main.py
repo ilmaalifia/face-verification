0e3f8aeb8de6f1cc30aadc78df662a089fbdd17b
ilmaalifia <ilma.mahardika@gmail.com>
2025-09-07 13:49:37 +0700

    feat: Add processing time in API

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 
 from backend import routes
 
-app = FastAPI()
-
 app = FastAPI(title="Face Verification API")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 
 app.include_router(routes.router, prefix="/api/v1")
